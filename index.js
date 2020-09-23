cf7880a9762a3c67277a1c20982df6b891633dd6
Mark Ellis <markellisdev@gmail.com>
2020-09-23 14:41:10 -0500

    created bot app in slack configured in index.js

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ const axios = require('axios');
 const dotenv = require('dotenv');
 
 dotenv.config()
+
+const bot = new Slackbot({
+    token: `${process.env.BOT_TOKEN}`,
+    name: 'inspire-bot'
+})
+
+bot.on('start', () => {
+    const params = {
+        icon_emoji: ':robot_face:'
+    }
+
+    bot.postMessageToChannel(
+        'inspirational-messages',
+        'Get inspired while working with @inspirationalquotes',
+        params
+    );
+})
