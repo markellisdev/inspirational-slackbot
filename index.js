f411b158257851ab93d8be3f049837c7aa7b7de9
Mark Ellis <markellisdev@gmail.com>
2020-09-23 20:39:37 -0500

    added message and handler. bot posting to channel in response

--- index.js
+++ index.js
@@ -20,13 +20,93 @@ bot.on('start', () => {
         params
     );
     // post message to individual user
-    bot.postMessageToUser(
-        'ellisfirst',
-        'This is a message from your friendly inspirational bot. Be well!',
-        params
-    );
+    // bot.postMessageToUser(
+    //     'ellisfirst',
+    //     'This is a message from your friendly inspirational bot. Be well!',
+    //     params
+    // );
 })
 
+// Error Handling
 bot.on('error', (err) => {
     console.log(err);
 })
+
+// Message Handler
+bot.on('message', (data) => {
+    if(data.type !== 'message') {
+        return;
+    }
+    handleMessage(data.text);
+})
+
+let handleMessage = message => {
+    if(message.includes('inspire me')) {
+        inspireMe()
+    } else if(message.includes('help')) {
+        runHelp()
+    }
+}
+// function handleMessage(message) {
+//     if(message.includes('inspire me')) {
+//         inspireMe()
+//     } else if(message.includes(' help')) {
+//         runHelp()
+//     }
+// }
+
+
+let inspireMe = () => {
+    axios.get('https://raw.githubusercontent.com/BolajiAyodeji/inspireNuggets/master/src/quotes.json')
+        .then(res => {
+            const quotes = res.data;
+            const random = Math.floor(Math.random() * quotes.length);
+            const quote = quotes[random].quote
+            const author = quotes[random].author
+
+            const params = {
+                icon_emoji: ':male-technologist:'
+            }
+            
+            bot.postMessageToChannel(
+                'inspirational-messages',
+                `:zap: ${quote} - *${author}*`,
+                params
+            )
+        })
+        
+}
+
+// function inspireMe() {
+//     axios.get('https://raw.githubusercontent.com/BolajiAyodeji/inspireNuggets/master/src/quotes.json')
+//       .then(res => {
+//             const quotes = res.data;
+//             const random = Math.floor(Math.random() * quotes.length);
+//             const quote = quotes[random].quote
+//             const author = quotes[random].author
+
+//             const params = {
+//                 icon_emoji: ':male-technologist:'
+//             }
+        
+//             bot.postMessageToChannel(
+//                 'random',
+//                 `:zap: ${quote} - *${author}*`,
+//                 params
+//             );
+
+//       })
+// }
+
+function runHelp() {
+    const params = {
+        icon_emoji: ':question:'
+    }
+
+    bot.postMessageToChannel(
+        'random',
+        `Type *@inspirenuggets* with *inspire me* to get an inspiring techie quote, *random joke* to get a Chuck Norris random joke and *help* to get this instruction again`,
+        params
+    );
+}
+
